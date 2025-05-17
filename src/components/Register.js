f9e3982798f9e2d0a068eb3e39d7900cf4df2340
deadsnxw <maks.roskoshenko@gmail.com>
2025-05-17 16:54:31 +0300

    implemented user/seller check

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,22 @@
 import "../styles/Login.css";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 export default function Register() {
     const navigate = useNavigate();
+    const [isChecked, setIsChecked] = useState(false);
 
-    const handleRegister = () => {
-        // register logic
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    }
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        if (!isChecked) {
+            console.log("User registered.")
+        } else {
+            console.log("Seller registered.")
+        }
     }
 
     return (
@@ -15,7 +26,11 @@ export default function Register() {
                 <input type="text" placeholder="Username"/>
                 <input type="email" placeholder="Email"/>
                 <input type="password" placeholder="Password"/>
-                <button type="submit" onClick={() => handleRegister()}>Register</button>
+                <button onClick={(event) => handleRegister(event)}>Register</button>
+                <div>
+                    <input type="checkbox" id="terms" checked={isChecked} onChange={handleCheckboxChange}/>
+                    <label htmlFor="terms">I'm a seller</label>
+                </div>
                 <span onClick={() => navigate("/Login")}>Already have an account ?</span>
             </form>
         </div>
