37ad505590a7c51295dafac5dcac65192970703e
Kozak Bogdan <bkozak04@gmail.com>
2025-05-23 12:59:01 +0300

    modif server and api routes

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -4,21 +4,21 @@ const usersController = require('../controllers/usersController');
 function router(req, res) {
   const { url, method } = req;
 
-  if (url === '/login' && method === 'POST') {
+  if (url === '/api/login' && method === 'POST') {
     return usersController.login(req, res);
   }
 
-  else if (url === '/user/' && method === 'GET') {
+  else if (url.startsWith('/api/user/') && method === 'GET') {
 		const id = url.split('/')[2];
 
 		return usersController.getProfile(req, res, id);
   }
 
-	else if ((url === '/') && method === 'GET') {
+	else if ((url === '/api/main') && method === 'GET') {
     return productController.getAllProducts(req, res);
   }
 
-  else if (url.startsWith('/product/') && method === 'GET') {
+  else if (url.startsWith('/api/product/') && method === 'GET') {
     const id = url.split('/')[2];
     return productController.getProductById(req, res, id);
   }
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,51 @@
+// server.js (єдиний файл)
 const http = require('http');
-const port = 5000;
+const fs = require('fs');
+const path = require('path');
 
+const port = 5000;
+const buildPath = path.join(__dirname, 'build');
 const router = require('./routes/router');
 
+const mimeTypes = {
+  '.js': 'application/javascript',
+  '.css': 'text/css',
+  '.html': 'text/html',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+};
+
 const server = http.createServer((req, res) => {
-  router(req, res);
+  if (req.url.startsWith('/api')) {
+    router(req, res);
+  } else {
+    let filePath = path.join(buildPath, req.url === '/' ? 'index.html' : req.url);
+    if (!path.extname(filePath)) {
+      filePath = path.join(filePath, 'index.html');
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        fs.readFile(path.join(buildPath, 'index.html'), (err2, indexData) => {
+          if (err2) {
+            res.writeHead(500);
+            res.end('Server error');
+            return;
+          }
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(indexData);
+        });
+      } else {
+        const ext = path.extname(filePath);
+        const mimeType = mimeTypes[ext] || 'application/octet-stream';
+        res.writeHead(200, { 'Content-Type': mimeType });
+        res.end(data);
+      }
+    });
+  }
 });
 
 server.listen(port, () => {
