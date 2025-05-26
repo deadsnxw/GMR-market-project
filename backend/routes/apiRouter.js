477e1df6220e54efd62e3b90ffd531cc11de93ba
Kozak Bogdan <bkozak04@gmail.com>
2025-05-26 19:17:23 +0300

    modified apiRouter

--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -1,68 +1,72 @@
-const productController = require('../controllers/productsController');
+const productsController = require('../controllers/productsController');
 const usersController = require('../controllers/usersController');
 
+const staticRoutes = {
+  '/api/login': usersController.login,
+  '/api/registration': usersController.registration,
+  '/api/balance': usersController.balance,
+  '/api/main': productsController.getAllProducts,
+  '/api/new': productsController.create,
+};
+
+const staticRoutesMethods = {
+  '/api/login': ['POST'],
+  '/api/registration': ['POST'],
+  '/api/balance': ['PATCH'],
+  '/api/main': ['GET'],
+  '/api/new': ['POST'],
+};
+
+const dynamicRoutes = [
+  {
+    method: 'GET',
+    regex: /^\/api\/user\/([^/]+)$/,
+    handler: (req, res, params) => usersController.getProfile(req, res, params[0])
+  },
+  {
+    method: 'PATCH',
+    regex: /^\/api\/user\/([^/]+)$/,
+    handler: (req, res, params) => usersController.patch(req, res, params[0])
+  },
+  {
+    method: 'GET',
+    regex: /^\/api\/product\/([^/]+)$/,
+    handler: (req, res, params) => productsController.getProductById(req, res, params[0])
+  },
+  {
+    method: 'POST',
+    regex: /^\/api\/product\/([^/]+)$/,
+    handler: (req, res, params) => productsController.buy_check(req, res, params[0])
+  },
+  {
+    method: 'DELETE',
+    regex: /^\/api\/product\/([^/]+)$/,
+    handler: (req, res, params) => productsController.remove(req, res, params[0])
+  },
+  {
+    method: 'PATCH',
+    regex: /^\/api\/product\/([^/]+)$/,
+    handler: (req, res, params) => productsController.patch(req, res, params[0])
+  }
+];
+
 function apiRouter(req, res) {
-  const { url, method } = req;
+  const method = req.method;
+  const url = req.url;
 
-  // DONE
-  if (url === '/api/login' && method === 'POST') {    
-    return usersController.login(req, res);
+  if (staticRoutes[url]) {
+    if (staticRoutesMethods[url].includes(method)) {
+      return staticRoutes[url](req, res);
+    }
   }
 
-  // DONE
-  else if (url.startsWith('/api/user/') && method === 'GET') {
-    const id = url.split('/')[3];
-    return usersController.getProfile(req, res, id);
-  }
-  
-  // DONE
-  else if ((url === '/api/main') && method === 'GET') {
-    return productController.getAllProducts(req, res);
-  }
-
-  // DONE
-  else if (url.startsWith('/api/product/') && method === 'GET') {
-    const id = url.split('/')[3];
-    return productController.getProductById(req, res, id);
-  }
-
-  // DONE
-  else if (url.startsWith('/api/product') && method === 'POST') {
-    const id = url.split('/')[3];
-    return productController.buy_check(req, res, id);
-  }
-
-  // DONE
-  else if (url.startsWith('/api/product') && method === 'DELETE') {
-    const id = url.split('/')[3];
-    return productController.remove(req, res, id);
-  }
-
-  // DONE
-  else if (url === ('/api/new') && method === 'POST') {
-    return productController.create(req, res);
-  }
-
-  // DONE
-  else if (url.startsWith('/api/product') && method === 'PATCH') {
-    const id = url.split('/')[3];
-    return productController.patch(req, res, id);
-  }
-
-  // DONE
-  else if (url === ('/api/balance') && method === 'PATCH') {
-    return usersController.balance(req, res);
-  }
-
-  // DONE
-  else if (url === ('/api/registration') && method === 'POST') {
-    return usersController.registration(req, res);
-  }
-
-  // DONE
-  else if (url.startsWith('/api/user') && method === 'PATCH') {
-    const id = url.split('/')[3];
-    return usersController.patch(req, res, id)
+  for (const route of dynamicRoutes) {
+    if (route.method === method) {
+      const match = url.match(route.regex);
+      if (match) {
+        return route.handler(req, res, match.slice(1));
+      }
+    }
   }
 
   res.statusCode = 404;
